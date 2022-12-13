4c89ba5a2eeceb593965753914b04dc6e0087f53
JulianAlv <Julian.Alvarez@fundacion-jala.org>
2022-12-12 21:07:36 -0500

    Agrego Query method a pedido e inserto pedido en la vista del cliente, con algunos triggers funcionales

--- src/main/java/com/example/demo/entity/Pedido.java
+++ src/main/java/com/example/demo/entity/Pedido.java
@@ -1,10 +1,15 @@
 package com.example.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.Value;
 
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.sql.Date;
 
 
@@ -23,24 +28,30 @@ public class Pedido {
     @Column
     private Date fecha;
 
-
-    @Column(length = 50, nullable = false)
+    @Min(value = 1, message = "Valor minimo 1")
+    @Column(name = "cantidad_total")
     private int cantidad;
 
     @NotEmpty(message = "El campo direccion no puede ser vacio")
     @Column(length = 50, nullable = false)
     private String direccion;
 
-    @Column(length = 50, nullable = false)
-    private int descuento;
-
     @ManyToOne
+    @NotNull(message = "Seleccionar un usuario")
     @JoinColumn(name = "id")
+    @JsonBackReference
     private Product product;
 
     @ManyToOne
+    @NotNull(message = "Seleccionar una forma de pago")
     @JoinColumn(name = "idforma_de_pago")
+    @JsonBackReference
     private Forma_de_pago forma_de_pago;
+
+    @ManyToOne
+    @JoinColumn(name = "idusers")
+    @JsonBackReference
+    private User user;
 
 
 }
--- src/main/java/com/example/demo/repository/PedidoRepository.java
+++ src/main/java/com/example/demo/repository/PedidoRepository.java
@@ -2,8 +2,15 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+    @Query("SELECT pe FROM Pedido pe WHERE pe.user.id = :id")
+    List<Pedido> getPedidoByUser(@Param("id") Long id);
 }
